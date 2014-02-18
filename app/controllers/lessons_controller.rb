37cb74653e9cadf7cea50635c8eb5156762403d0
Alex <Alexrosh92@mail.ru>
2014-02-19 03:13:11 +0400

    correct yamaps javascript

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -2,7 +2,7 @@ class LessonsController < ApplicationController
   # GET /lessons
   # GET /lessons.json
   def index
-    @lessons = Lesson.all
+    @lessons = Lesson.paginate(:page => params[:page], :per_page => 3)
 
     respond_to do |format|
       format.html # index.html.erb
