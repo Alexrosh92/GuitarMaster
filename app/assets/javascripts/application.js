8c41678948da2fb71352883d87fa3635e97f9e45
Alex <Alexrosh92@mail.ru>
2014-03-14 16:59:43 +0400

    name file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -40,5 +40,12 @@ $(function() {
         myMap.controls.add('zoomControl');
         myMap.geoObjects.add(myPlacemark);
     }
-
+    $('.custom-file-input').on('change', function() {
+        realVal = $(this).val();
+        lastIndex = realVal.lastIndexOf('\\') + 1;
+        if(lastIndex !== -1) {
+            realVal = realVal.substr(lastIndex);
+            $(this).prev('.mask').find('.fileInputText').val(realVal);
+        }
+    })
 });
